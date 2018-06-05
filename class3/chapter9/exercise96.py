9668845538613d8e46a73a6ede432edbef518561
Philip Ulrich <philip@ulrich.life>
2018-06-05 18:00:48 +0000

    adding 9.6

--- class3/chapter9/exercise96.py
+++ class3/chapter9/exercise96.py
@@ -10,7 +10,7 @@ __author__ = "Your Name"
 # Grading Guidelines:
 # - No answer variables needed. Grading script will call function.
 # - Function 'is_abecedarian' should return boolean True/False depending on if
-# only the letters specified are in the word. 
+# the letters are in alphabetical order.
 #
 # 1. Write a function called is_abecedarian that returns True if the letters in
 # a word appear in alphabetical order (double letters are ok).
@@ -19,7 +19,7 @@ __author__ = "Your Name"
 def is_abecedarian(word):
     previous_letter = word[0]
     for letter in word:
-        if ord(letter) <= ord(previous_letter):
+        if ord(letter) < ord(previous_letter):
             return False
         previous_letter = letter
     return True
--- class3/chapter9/grade.py
+++ class3/chapter9/grade.py
@@ -169,9 +169,9 @@ def grade(assignment):
                 if str_in.lower() == 'y':
                     menu()
     elif assignment == '9.6':
-        if exercise91.main() == ['counterdemonstrations', 'hyperaggressivenesses', 'microminiaturizations']:
+        if exercise96.is_abecedarian('abbcdef') and not exercise96.is_abecedarian('cndsfsd'):
             db['submitted'][assignment] = True
-            submit('exercise91.py')
+            submit('exercise96.py')
             str_in = input("Exercise answer correct and submitted. Would you like to submit another? (y/n): ")
             if str_in.lower() == 'y':
                 menu()
@@ -179,7 +179,7 @@ def grade(assignment):
             str_in = input('The exercise answer was incorrect. Did you still want to submit it? (y/n): ')
             if str_in.lower() == 'y':
                 db['submitted'][assignment] = True
-                submit('exercise91.py')
+                submit('exercise96.py')
                 str_in = input("Exercise submitted. Would you like to submit another? (y/n): ")
                 if str_in.lower() == 'y':
                     menu()
