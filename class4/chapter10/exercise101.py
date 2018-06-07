b911de5e5292a2ef6c0e3af911b0cb2414373333
Philip Ulrich <philip@ulrich.life>
2018-06-07 19:01:46 +0000

    adding 10.1 and other changes

--- class4/chapter10/exercise101.py
+++ class4/chapter10/exercise101.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
+__author__ = "Your Name"
+
 ###############################################################################
 #
-print("\nExercise 10.1\n")
+# Exercise 10.1
 #
-# Question 1
+#
+# Grading Guidelines:
+# - No answer variable is needed. Grading script will call function.
+# - Function "nested_sum" should output a integer that is a sum of all integers
+# in the nested lists.
+#
 # 1. Write a function called nested_sum that takes a list of lists of integers 
 # and adds up the elements from all of the nested lists. For example:
 #
@@ -17,7 +24,3 @@ def nested_sum(li):
     for elem in li:
         result += sum(elem)
     return result
-
-
-t = [[1, 2], [3], [4, 5, 6]]
-print(nested_sum(t))
